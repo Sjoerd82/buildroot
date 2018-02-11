050e0323ca59618b3b348114bdf89e2d28976d41
Sjoerd82 <srg.venema@gmail.com>
2018-02-11 13:40:05 +0100

    via commit -a

--- buildroot-2017.08/board/arctura/overlay_hu/root/start_wlan0.sh
+++
@@ -1,8 +0,0 @@
-# Samba user
-echo -e "kungfu\nkungfu" | smbpasswd -s -a root
-
-# Wifi
-wpa_supplicant -B -i wlan0 -W -D nl80211, wext -c /etc/wpa_supplicant.conf
-wpa_cli -B -i wlan0 -p /var/run/wpa_supplicant -a /sbin/wpa_action
-dhcpcd -q -w
-ip addr
